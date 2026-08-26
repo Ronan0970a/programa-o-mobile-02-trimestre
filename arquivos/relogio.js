d77e08b8c065fcd9388bddac6b17445991e55aa1
Ronan0970a <ronan.soares@escola.pr.gov.br>
2026-08-26 20:24:54 -0300

    Add files via upload

---
+++ arquivos/relogio.js
@@ -0,0 +1,12 @@
+var hora = 18;
+
+console.log("Agora são exatamente " + hora + " horas.");
+
+if (hora <= 12) {
+    console.log("tenha um otimo dia");
+} else if (hora <= 18) {
+    console.log("tenha uma otima tarde");
+} else {
+    console.log("tenha uma otima noite");
+}
+
---
+++ arquivos/votação.js
@@ -0,0 +1,5 @@
+var idade = 22
+if (idade < 16){
+    console.log('não vota')
+} else{
+console.log("vota")}
